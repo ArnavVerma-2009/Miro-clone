7e8106a4d77d5e0c6f9b57d33c97dacba86b9d1a
ARNAVVERMAbang <arnav.verma.feb.2009@gmail.com>
2024-02-23 01:03:52 +0530

    Bugs : Search and favoriting functionality fixed for production.

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -5,22 +5,20 @@ import { UserButton, useOrganization } from "@clerk/nextjs";
 import Image from "next/image";
 import { EmptyOrg } from "./_components/EmptyOrg";
 import { BoardList } from "./_components/BoardList";
+import { useSearchParams } from "next/navigation";
 
-interface DashboardPageProps {
-  searchParams: {
-    search?: string;
-    favorites?: string;
-  };
-}
-const DashboardPage = ({ searchParams }: DashboardPageProps) => {
+const DashboardPage = () => {
   const { organization } = useOrganization();
+  const searchParams = useSearchParams();
 
+  const search = searchParams.get("search") || undefined;
+  const favorites = searchParams.get("favorites") || undefined;
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
       {!organization ? (
         <EmptyOrg />
       ) : (
-        <BoardList orgId={organization.id} query={searchParams} />
+        <BoardList orgId={organization.id} query={{ search, favorites }} />
       )}
     </div>
   );
